aa98fc1f2ce7f09d6d911762d852d7eecfae1d29
juasesan <jssa132@gmail.com>
2023-01-27 10:47:59 -0500

    Update first point detection

--- camera.py
+++ camera.py
@@ -4,7 +4,9 @@ import cv2
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.neighbors import kneighbors_graph
 import seaborn as sns
+import math
 
 
 # Configure depth and color streams of the camera
@@ -35,11 +37,14 @@ def take_picture():
 
 
 def get_points(color, depth=None):
+    color = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+    #color = cv2.resize(color, (400, 400))
     _,thresh2 = cv2.threshold(color,100,255,cv2.THRESH_BINARY_INV)
     
     points = np.transpose(np.nonzero(thresh2))
     
-    clustering = AgglomerativeClustering(n_clusters=40, linkage="average")
+    knn_graph = kneighbors_graph(points, 30, include_self=False)
+    clustering = AgglomerativeClustering(n_clusters=30, linkage="ward", connectivity=knn_graph)
     clustering.fit(points)
 
     df = pd.DataFrame(points)
@@ -49,15 +54,62 @@ def get_points(color, depth=None):
 
     dfg = df.groupby('clusters').agg({'X':[np.mean], 'Y':[np.mean]}).reset_index()
     dfg.columns = ['cluster', 'X', 'Y']
+    dfg['X'] = dfg['X'] / 400.0
+    dfg['Y'] = (dfg['Y'] + 400.0) / 400.0
     dfg['Z'] = 0.0
 
-    return dfg
+    points_list = list(dfg[['X', 'Y']].itertuples(index=False, name=None))
+
+    list_sorted = [points_list[0]]
+    points_list.remove(points_list[0])
+    node_points = []
+
+    flag = True
+
+    while flag:
+        point1 = list_sorted[-1]
+        distances = []
+        points = []
+        
+        for point2 in points_list:
+            distance = math.dist(point1, point2)
+            distances.append(distance)
+            points.append(point2)
+            
+        min_dist_1 = min(distances)
+        min_idx_1 = distances.index(min_dist_1)
+        
+        distances.remove(min_dist_1)
+        
+        try:
+            min_dist_2 = min(distances)
+            min_idx_2 = distances.index(min_dist_2)
+
+            factor = min_dist_2 / min_dist_1
+
+            if (factor > 1) and (factor <= 1.3) and (list_sorted[-2] not in node_points) and (list_sorted[-3] not in node_points):
+                node_points.append(point1)
+                
+        except:
+            pass
+        
+        list_sorted.append(points[min_idx_1])
+        points_list.remove(points[min_idx_1])
+        
+        if not points_list:
+            flag = False
+
+    df_points = pd.DataFrame(list_sorted, columns=['X', 'Y'])
+
+    return df_points
 
 
 def main():
     depth_img, color_img = take_picture()
     
     wire_points = get_points(color_img)
+    #img = cv2.imread('./Data/sample images/1.jpg')
+    #wire_points = get_points(img)
 
     print(wire_points)
     wire_points.to_csv('./points_xyz.csv')
