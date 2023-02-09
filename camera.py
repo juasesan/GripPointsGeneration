a2df8f163f1bbc9c165ec4fefd21b58cd30213c1
juasesan <jssa132@gmail.com>
2023-02-09 11:17:10 -0500

    Update camera node

--- camera.py
+++ camera.py
@@ -7,23 +7,25 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.neighbors import kneighbors_graph
 import seaborn as sns
 import math
+import time
 
-
-# Configure depth and color streams of the camera
-pipeline = rs.pipeline()
-config = rs.config()
-
-config.enable_stream(rs.stream.depth, 320, 240, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-
-
-aligned_stream = rs.align(rs.stream.color) # alignment between color and depth
-point_cloud = rs.pointcloud()
-
-# Start streaming
-profile = pipeline.start(config)
+start = time.time()
 
 def take_picture():
+    # Configure depth and color streams of the camera
+    pipeline = rs.pipeline()
+    config = rs.config()
+
+    config.enable_stream(rs.stream.depth, 320, 240, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+
+
+    aligned_stream = rs.align(rs.stream.color) # alignment between color and depth
+    point_cloud = rs.pointcloud()
+
+    # Start streaming
+    profile = pipeline.start(config)
+    
     frames = pipeline.wait_for_frames()
     depth_frame = frames.get_depth_frame()
     color_frame = frames.get_color_frame()
@@ -38,13 +40,13 @@ def take_picture():
 
 def get_points(color, depth=None):
     color = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
-    #color = cv2.resize(color, (400, 400))
+    color = cv2.resize(color, (320, 240))
     _,thresh2 = cv2.threshold(color,100,255,cv2.THRESH_BINARY_INV)
     
     points = np.transpose(np.nonzero(thresh2))
     
-    knn_graph = kneighbors_graph(points, 30, include_self=False)
-    clustering = AgglomerativeClustering(n_clusters=30, linkage="ward", connectivity=knn_graph)
+    knn_graph = kneighbors_graph(points, 100, include_self=False)
+    clustering = AgglomerativeClustering(n_clusters=100, linkage="ward", connectivity=knn_graph)
     clustering.fit(points)
 
     df = pd.DataFrame(points)
@@ -52,18 +54,27 @@ def get_points(color, depth=None):
     df.columns = ['Y', 'X', 'clusters']
     df['Y'] = df['Y'] * (-1)
 
-    dfg = df.groupby('clusters').agg({'X':[np.mean], 'Y':[np.mean]}).reset_index()
-    dfg.columns = ['cluster', 'X', 'Y']
-    dfg['X'] = dfg['X'] / 400.0
-    dfg['Y'] = (dfg['Y'] + 400.0) / 400.0
-    dfg['Z'] = 0.0
+    cluster_df = df.groupby('clusters').agg({'X':[np.mean], 'Y':[np.mean]}).reset_index()
+    cluster_df.columns = ['cluster', 'X', 'Y']
+    cluster_df['X'] = cluster_df['X'] / 400.0
+    cluster_df['Y'] = (cluster_df['Y'] + 400.0) / 400.0
+    cluster_df['Z'] = 0.0
 
-    points_list = list(dfg[['X', 'Y']].itertuples(index=False, name=None))
+    points_list = list(cluster_df[['X', 'Y']].itertuples(index=False, name=None))
+
+    for i in range(len(points_list)):       # Rounds each centroid point to the nearest point on its cluster
+        cluster_df = df.loc[df['clusters'] == i]
+        cluster_list = list(cluster_df[['X', 'Y']].itertuples(index=False, name=None))
+        dist_list = []
+        for pt in cluster_list:
+            dist_list.append(math.dist(points_list[i], pt))
+        idx = dist_list.index(min(dist_list))
+        points_list[i] = cluster_list[idx]
 
     list_sorted = [points_list[0]]
     points_list.remove(points_list[0])
     node_points = []
-
+    
     flag = True
 
     while flag:
@@ -79,43 +90,63 @@ def get_points(color, depth=None):
         min_dist_1 = min(distances)
         min_idx_1 = distances.index(min_dist_1)
         
-        distances.remove(min_dist_1)
-        
-        try:
-            min_dist_2 = min(distances)
-            min_idx_2 = distances.index(min_dist_2)
-
-            factor = min_dist_2 / min_dist_1
-
-            if (factor > 1) and (factor <= 1.3) and (list_sorted[-2] not in node_points) and (list_sorted[-3] not in node_points):
-                node_points.append(point1)
-                
-        except:
-            pass
-        
         list_sorted.append(points[min_idx_1])
         points_list.remove(points[min_idx_1])
         
         if not points_list:
             flag = False
 
-    df_points = pd.DataFrame(list_sorted, columns=['X', 'Y'])
+    distances_list = []
+    for i in range(len(list_sorted)):
+        if not i == (len(list_sorted) - 1):
+            dist = math.dist(list_sorted[i], list_sorted[i+1])
+            distances_list.append(dist)
+
+    distances = pd.Series(distances_list.copy())
+    threshold = distances.mean() + distances.std()
+    first_pt_idx = distances_list.index(distances.loc[distances > threshold].iloc[0])
+
+    list_sorted2 = [list_sorted[first_pt_idx]]
+    list_sorted.remove(list_sorted[first_pt_idx])
+
+    flag = True
+
+    while flag:
+        point1 = list_sorted2[-1]
+        distances = []
+        points = []
+        
+        for point2 in list_sorted:
+            distance = math.dist(point1, point2)
+            distances.append(distance)
+            points.append(point2)
+            
+        min_idx_1 = distances.index(min(distances))
+        
+        list_sorted2.append(points[min_idx_1])
+        list_sorted.remove(points[min_idx_1])
+        
+        if not list_sorted:
+            flag = False
+    
+    df_points = pd.DataFrame(list_sorted2, columns=['X', 'Y'])
 
     return df_points
 
 
 def main():
-    depth_img, color_img = take_picture()
+    #depth_img, color_img = take_picture()
     
-    wire_points = get_points(color_img)
-    #img = cv2.imread('./Data/sample images/1.jpg')
-    #wire_points = get_points(img)
+    #wire_points = get_points(color_img)
+    img = cv2.imread('./Data/sample images/1.jpg')
+    wire_points = get_points(img)
 
     print(wire_points)
-    wire_points.to_csv('./points_xyz.csv')
-
+    #wire_points.to_csv('./points_xyz.csv')
 
 if __name__ == '__main__':
     main()
+    end = time.time()
+    print(end - start)
 
         
